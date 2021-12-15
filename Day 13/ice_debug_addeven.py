c617bb9fdfa43af424e3a62808a19e801bbb21a1
Imam <imam0067@gmail.com>
2021-12-15 16:07:54 +0700

    Day 13

---
+++ Day 13/ice_debug_addeven.py
@@ -0,0 +1,10 @@
+"""
+Interactive Coding Exercise : Debug Odd and Even Numbers
+"""
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
